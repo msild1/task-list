7c0261a04ccb9168961821bff5ba260379f7e3c5
msild1 <markus.sild.001@voco.ee>
2022-10-05 11:38:31 +0300

    As a user I can filter task list [Delivers #183463483]

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const form  = document.querySelector('form')
 const taskInput = document.querySelector('#task')
 const taskList = document.querySelector('ul')
 const delAllTasks = document.querySelector('#del-tasks')
+const filterTasksButton = document.querySelector('#filterTaskButton')
+const filterTasksInput = document.querySelector('#filterTaskInput')
+const cancelFilters = document.querySelector('#cancelFilter')
 
 form.addEventListener('submit', addTask)
 taskList.addEventListener('click', deleteTask)
 delAllTasks.addEventListener('click', deleteAllTasks)
+filterTasksButton.addEventListener('click', filterTasks)
+cancelFilters.addEventListener('click', cancelFilter)
 document.addEventListener('DOMContentLoaded', getTasks)
 
 function addTask(e){
@@ -90,3 +95,67 @@ function getTasks(){
         ul.appendChild(li)
     })
 }
+
+function filterTasks(){
+    while (taskList.firstChild){
+        taskList.removeChild(taskList.firstChild)
+    }
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+        let arrayLength = tasks.length;
+        let new_array = []
+
+        for (let i = 0; i < arrayLength; i++) {
+            console.log(tasks[i]);
+            const substring = (filterTasksInput.value);
+            if (tasks[i].includes(substring)) {
+                new_array.push(tasks[i])
+            }
+        }
+        console.log(new_array)
+
+        new_array.forEach((task) => {
+            const li = document.createElement('li')
+            li.appendChild(document.createTextNode(task))
+            li.className = 'collection-item'
+            const a = document.createElement('a')
+            a.appendChild(document.createTextNode('X'))
+            a.className = 'blue-text text-darken-2 secondary-content'
+            a.setAttribute('href', '#')
+            li.appendChild(a)
+            // add to list
+            const ul = document.querySelector('ul')
+            ul.appendChild(li)
+        })
+
+    }
+}
+function cancelFilter() {
+    while (taskList.firstChild){
+        taskList.removeChild(taskList.firstChild)
+    }
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((task) => {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(task))
+        li.className = 'collection-item'
+        const a = document.createElement('a')
+        a.appendChild(document.createTextNode('X'))
+        a.className = 'blue-text text-darken-2 secondary-content'
+        a.setAttribute('href', '#')
+        li.appendChild(a)
+        // add to list
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
+    })
+}
+
+
