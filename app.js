f096b245f14ffee66941658a26c0ed497fc6e266
msild1 <markus.sild.001@voco.ee>
2022-10-05 10:38:52 +0300

    As a user I can reload page and see actual data [Delivers #183440391]

--- app.js
+++ app.js
@@ -1,42 +1,92 @@
-const form = document.querySelector("#task-form")
-const taskInput = document.querySelector("#task")
-const taskList = document.querySelector("ul")
-const deleteAllTaskButton = document.querySelector("button")
+const form  = document.querySelector('form')
+const taskInput = document.querySelector('#task')
+const taskList = document.querySelector('ul')
+const delAllTasks = document.querySelector('#del-tasks')
 
-form.addEventListener("submit",addTask)
-taskList.addEventListener("click",deleteTask)
-deleteAllTaskButton.addEventListener("click",deleteAllTasks)
+form.addEventListener('submit', addTask)
+taskList.addEventListener('click', deleteTask)
+delAllTasks.addEventListener('click', deleteAllTasks)
+document.addEventListener('DOMContentLoaded', getTasks)
 
-function addTask(event){
-    // create task list
-    const li = document.createElement("li")
+function addTask(e){
+    // create list item
+    const li = document.createElement('li')
     li.appendChild(document.createTextNode(taskInput.value))
-    li.className = "collection-item"
-
-
-    const a = document.createElement("a")
-    a.appendChild(document.createTextNode("X"))
-    a.className = "blue-text text-darken-2 secondary-content"
-    a.setAttribute("href","#")
+    li.className = 'collection-item'
+    const a = document.createElement('a')
+    a.appendChild(document.createTextNode('X'))
+    a.className = 'blue-text text-darken-2 secondary-content'
+    a.setAttribute('href', '#')
     li.appendChild(a)
-
-    // add to task list
-    const ul = document.querySelector("ul")
+    // add to list
+    const ul = document.querySelector('ul')
     ul.appendChild(li)
-    event.preventDefault()
-    taskInput.value = ""
+    addTaskLS(taskInput.value)
+    taskInput.value = ''
+    e.preventDefault()
 }
 
-function deleteTask(event){
-   // delete task
-    if (event.target.textContent === "X"){
-        if (confirm("Are you sure?")) {
-            event.target.parentElement.remove()
+function deleteTask(e){
+    if(e.target.textContent == 'X'){
+        if(confirm('Are you sure to delete this task?')){
+            e.target.parentElement.remove()
+            deleteTaskLS(e.target.parentElement.textContent.slice(0,-1))
         }
     }
 }
-function deleteAllTasks(event){
-    if (event.target.textContent === "Delete all"){
-        while(taskList.firstChild) taskList.removeChild(taskList.firstChild);
+
+function deleteAllTasks(e){
+    //taskList.innerHTML = ''
+    while (taskList.firstChild){
+        taskList.removeChild(taskList.firstChild)
     }
+    localStorage.removeItem('tasks')
+}
+
+function addTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.push(task)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function deleteTaskLS(task) {
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((taskLS, taskIndex) => {
+        if(taskLS === task){
+            tasks.splice(taskIndex, 1)
+        }
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+function getTasks(){
+    let tasks
+    if(localStorage.getItem('tasks') === null){
+        tasks = []
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'))
+    }
+    tasks.forEach((task) => {
+        const li = document.createElement('li')
+        li.appendChild(document.createTextNode(task))
+        li.className = 'collection-item'
+        const a = document.createElement('a')
+        a.appendChild(document.createTextNode('X'))
+        a.className = 'blue-text text-darken-2 secondary-content'
+        a.setAttribute('href', '#')
+        li.appendChild(a)
+        // add to list
+        const ul = document.querySelector('ul')
+        ul.appendChild(li)
+    })
 }
